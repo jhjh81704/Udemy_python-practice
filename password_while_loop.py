d82e56b2ddda5521d03cdf7620d1ec80a27cc776
jhjh81704 <jhjh81704@gmail.com>
2019-04-25 20:52:10 +0800

    change to while > 0

--- password_while_loop.py
+++ password_while_loop.py
@@ -8,7 +8,7 @@ Created on Thu Apr 25 20:35:09 2019
 password = 'a123456'
 i = 3  # 剩餘機會
 
-while True:
+while i > 0:
     pwd = input('請輸入密碼: ')
     if pwd == password:
         print('登入成功 !')
@@ -17,5 +17,4 @@ while True:
     else:
         i -= 1
         print('密碼錯誤 !  還有', i, '次機會')
-        if i == 0:
-            break
+        
