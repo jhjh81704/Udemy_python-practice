0235c1c0c2e1c3e39c9520aedb57185555f2a77b
jhjh81704 <jhjh81704@gmail.com>
2019-04-25 18:22:47 +0800

    homework_update

--- BMI_cal.py
+++ BMI_cal.py
@@ -2,4 +2,15 @@ height = float(input('請輸入體重: '))
 tall = float(input('請輸入身高: '))
 BMI = height / tall**2
 
-print(BMI)
+if BMI < 18.5:
+	print('體重過輕')
+elif BMI >= 18.5 and BMI < 24:
+	print('正常範圍')
+elif BMI >= 24 and BMI < 27:
+	print('過重')
+elif BMI >= 27 and BMI < 30:
+	print('輕度肥胖')
+elif BMI >= 30 and  BMI < 35:
+	print('中度肥胖')
+else:
+	print('種度肥胖')
