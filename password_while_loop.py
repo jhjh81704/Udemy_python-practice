4985a710aff385a6ca6f9cda7194d90f39f6c24b
jhjh81704 <jhjh81704@gmail.com>
2019-04-25 21:00:00 +0800

    Add password lock

--- password_while_loop.py
+++ password_while_loop.py
@@ -9,12 +9,16 @@ password = 'a123456'
 i = 3  # 剩餘機會
 
 while i > 0:
+    i -= 1
     pwd = input('請輸入密碼: ')
     if pwd == password:
         print('登入成功 !')
         break # 離開 loop
         
     else:
-        i -= 1
-        print('密碼錯誤 !  還有', i, '次機會')
         
+        print('密碼錯誤 !')
+        if i > 0:
+            print('還有', i, '次機會')
+        else:
+            print('沒機會嘗試了! 要鎖帳號了')
